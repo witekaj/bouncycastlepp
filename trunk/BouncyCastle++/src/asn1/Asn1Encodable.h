e004112242e0f01c2866596160309b35b97c94f1
watsug <watsug@7733027f-963f-0410-b8fd-31a395116735>
2007-11-23 11:15:32 +0000

    initial import

---
+++ trunk/BouncyCastle++/src/asn1/Asn1Encodable.h
@@ -0,0 +1,83 @@
+// 
+// File:   Asn1Encodable.h
+// Author: watsug
+//
+// Created on November 23, 2007, 2:29 AM
+//
+
+#ifndef _ASN1ENCODABLE_H
+#define	_ASN1ENCODABLE_H
+
+//using System.IO;
+
+namespace Org {
+  namespace BouncyCastle {
+    namespace Asn1 {
+
+      class Asn1Encodable : IAsn1Convertible {
+
+      public:
+        static const char* Der;// = "DER";
+        static const char* Ber;// = "BER";
+
+        byte[] GetEncoded() {
+                MemoryStream bOut = new MemoryStream();
+                Asn1OutputStream aOut = new Asn1OutputStream(bOut);
+
+          aOut.WriteObject(this);
+
+          return bOut.ToArray();
+        }
+
+        byte[] GetEncoded(string encoding) {
+          if (encoding.Equals(Der)) {
+            MemoryStream bOut = new MemoryStream();
+            DerOutputStream dOut = new DerOutputStream(bOut);
+            dOut.WriteObject(this);
+            return bOut.ToArray();
+          }
+          return GetEncoded();
+        }
+
+        /**
+        * Return the DER encoding of the object, null if the DER encoding can not be made.
+        *
+        * @return a DER byte array, null otherwise.
+        */
+        byte[] GetDerEncoded() {
+          try {
+            return GetEncoded(Der);
+          } catch (IOException) {
+            return null;
+          }
+        }
+
+        virtual int GetHashCode() {
+          return ToAsn1Object().CallAsn1GetHashCode();
+        }
+
+        virtual bool Equals(object obj) {
+          if (obj == this)
+            return true;
+
+          IAsn1Convertible other = obj as IAsn1Convertible;
+
+          if (other == null)
+            return false;
+
+          Asn1Object o1 = ToAsn1Object();
+          Asn1Object o2 = other.ToAsn1Object();
+
+          return o1 == o2 || o1.CallAsn1Equals(o2);
+        }
+
+        public abstract Asn1Object ToAsn1Object();
+        }
+      }
+
+    } //namespace Asn1
+  } //namespace BouncyCastle
+} //namespace Org
+
+
+#endif	/* _ASN1ENCODABLE_H */
